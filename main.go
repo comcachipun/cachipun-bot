69e8297f0ee9900c6458e2c0d7e445cdf35a6b6c
Eduardo <e.laboral@xor.cl>
2021-07-15 23:20:55 -0400

    now is more difficult (no flags here sorry)

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-type Jugada uint8
+type Jugada int8
 
 type Outcome int8
 
@@ -20,8 +20,8 @@ var TIMES_TO_WIN = 10
 
 var Jugadas = [][]Jugada{
 	{PIEDRA, PAPEL, PAPEL, TIJERAS, TIJERAS, PIEDRA, TIJERAS, PAPEL, TIJERAS, PAPEL},
-	{PAPEL, TIJERAS, PAPEL, PAPEL, PAPEL, PAPEL, PAPEL, TIJERAS, PAPEL, TIJERAS},
-	{TIJERAS, PIEDRA, PAPEL, PAPEL, PAPEL, PAPEL, PAPEL, PAPEL, PAPEL, PAPEL},
+	{PAPEL, TIJERAS, PAPEL, PAPEL, PIEDRA, PAPEL, PAPEL, TIJERAS, PAPEL, TIJERAS},
+	{TIJERAS, PIEDRA, PAPEL, TIJERAS, PAPEL, PIEDRA, PIEDRA, PAPEL, TIJERAS, PAPEL},
 }
 
 var NombresJugadas map[Jugada]string = map[Jugada]string{
@@ -32,22 +32,23 @@ var NombresJugadas map[Jugada]string = map[Jugada]string{
 
 const (
 	PIEDRA  Jugada = 1
-	PAPEL          = 2
-	TIJERAS        = 3
+	PAPEL   Jugada = 2
+	TIJERAS Jugada = 3
 )
 
 const (
-	WIN  Outcome = -1
-	LOSE         = 0
-	DRAW         = 1
+	LOSE Outcome = -1
+	DRAW Outcome = 0
+	WIN  Outcome = 1
 )
 
 func (j Jugada) Play(j2 Jugada) Outcome {
-	if j == j2 {
+	switch j - j2 {
+	case 0:
 		return DRAW
-	} else if j-j2 == 1 || int(j-j2) == -2 {
+	case 1, -2:
 		return WIN
-	} else {
+	default:
 		return LOSE
 	}
 }
@@ -58,16 +59,17 @@ func Handle(conn net.Conn) {
 		conn.SetDeadline(time.Now().Add(240 * time.Second))
 		msg := make([]byte, 2)
 		WelcomeMessage := fmt.Sprintf(
-			"¡Juguemos Cachipún!\nSi ganas %d veces te ganas un premio.\n\n",
+			"**[comcachipun/cachipun-bot]**\n¡Juguemos Cachipún!\nSi ganas %d veces te ganas un premio.\n\n",
 			TIMES_TO_WIN,
 		)
 		conn.Write([]byte(WelcomeMessage))
 		wins := 0
 		turn := 1
+		gameStyle := rand.Int() % 3
 		for {
 			if wins == TIMES_TO_WIN {
-				conn.Write([]byte(fmt.Sprintf("¡Ganaste!\n\nLa Flag es %s", os.Getenv("FLAG"))))
-				break
+				conn.Write([]byte(fmt.Sprintf("¡Ganaste!\n\nLa Flag es %s=\n\n", os.Getenv("FLAG"))))
+				return
 			}
 			conn.Write([]byte(fmt.Sprintf("[Turno %d / Perdí: %d]\nEscribe tu opción:\n1) PIEDRA\n2) PAPEL\n3) TIJERAS\n\n>>> ", turn, wins)))
 			n, err := conn.Read(msg)
@@ -91,7 +93,7 @@ func Handle(conn net.Conn) {
 				return
 			}
 			opt := Jugada(resp)
-			jugada := Jugadas[rand.Int()%3][(turn-1)%TIMES_TO_WIN]
+			jugada := Jugadas[gameStyle][(turn-1)%TIMES_TO_WIN]
 			conn.Write([]byte(fmt.Sprintf("Jugaste %s.\n", NombresJugadas[opt])))
 			conn.Write([]byte(fmt.Sprintf("Yo jugué %s.\n\n", NombresJugadas[jugada])))
 			switch opt.Play(jugada) {
